60f0c7c6b946956fa4d5320471f3c8d9d3d0d074
João Paulo <joaopaulotraguetta14@gmail.com>
2024-09-05 23:46:13 -0300

    Hero section

--- ProjetoOrigina/scripts/homescript.js
+++ ProjetoOrigina/scripts/homescript.js
@@ -8,3 +8,34 @@ function menuShow() {
         document.querySelector('.icon').src = "assets/close_white_36dp.svg"
     }
 }
+
+const historia = document.querySelector('#historia-list')
+historia.addEventListener('click', () => {
+    console.log('Redireciona para "Historia"!')
+})
+
+const editoriais = document.querySelector('#editoriais-list')
+    editoriais.addEventListener('click', () => {
+    console.log('Redireciona para "Editoriais"!')
+})
+
+const faleConosco = document.querySelector('#faleconosco-list')
+faleConosco.addEventListener('click', () => {
+console.log('Redireciona para "Fale Conosco"!')
+})
+
+const oGrupo = document.querySelector('#ogrupo-list')
+oGrupo.addEventListener('click', () => {
+console.log('Redireciona para "O Grupo"!')
+})
+
+const conceito = document.querySelector('#conceito-list')
+    conceito.addEventListener('click', () => {
+    console.log('Redireciona para "Conceito"!')
+})
+
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem',window.scrollY > 500)
+})
