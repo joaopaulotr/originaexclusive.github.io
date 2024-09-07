d1b63896312b5ebb0ea032e9f630b2dd57b487b2
Leocm123 <leonardocamilottimoreno@gmail.com>
2024-09-07 11:49:31 -0300

    resp bolas

--- ProjetoOrigina/scripts/slider.js
+++ ProjetoOrigina/scripts/slider.js
@@ -1,19 +1,18 @@
+var radio = document.querySelector(".manual-btn");
+var cont = 1;
 
-var radio = document.querySelector('.manual-btn')
-var cont = 1
-
-document.getElementById('radio1').checked = true
+document.getElementById("radio1").checked = true;
 
 setInterval(() => {
-    proximaImg()
-}, 5000)
+  proximaImg();
+}, 5000);
 
-function proximaImg(){
-    cont++
+function proximaImg() {
+  cont++;
 
-    if(cont > 3){
-        cont = 1 
-    }
+  if (cont > 3) {
+    cont = 1;
+  }
 
-    document.getElementById('radio'+cont).checked = true
+  document.getElementById("radio" + cont).checked = true;
 }
